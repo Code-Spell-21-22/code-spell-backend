8e384f234453e391c21845d4184c98dd5c05e5b5
Hugo1307 <hugomiguel_2001_@sapo.pt>
2022-04-10 22:13:10 +0100

    Initial commit

---
+++ src/main/java/pt/ua/deti/codespell/codespellbackend/CodeSpellBackendApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.codespell.codespellbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSpellBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSpellBackendApplication.class, args);
+    }
+
+}
