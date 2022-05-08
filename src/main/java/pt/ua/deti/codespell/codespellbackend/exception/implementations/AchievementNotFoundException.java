c855d8aa81afb83692be49f9661afc5a402f5871
artur-romao <arturomao@gmail.com>
2022-05-08 01:00:15 +0100

    Revert "halfway user controller - model, service and repository updated"
    This reverts commit be8d6ba00b933b1c42b4e508d1306868f9b42c34.

--- src/main/java/pt/ua/deti/codespell/codespellbackend/exception/implementations/AchievementNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package pt.ua.deti.codespell.codespellbackend.exception.implementations;
-
-public class AchievementNotFoundException extends RuntimeException {
-
-    public AchievementNotFoundException(String message) {
-        super(message);
-    }
-
-}
